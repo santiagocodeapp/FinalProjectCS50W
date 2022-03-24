b17e289888cdc9a9a27c4b53d1cd13df5f8588c1
Santiago Nessy <santiago.codeapp@gmail.com>
2022-03-24 19:41:42 -0400

    Final Project

---
+++ MyReport/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import URLPattern, path
+from . import views
+
+# app_name = 'coach'
+urlpatterns = [
+
+    path("", views.index, name = "index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("team/<str:team>", views.team, name="team"),
+    path("send/report", views.send_report, name="send-report"),
+
+]
